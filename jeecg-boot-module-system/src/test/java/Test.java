dc34cb6e6a7484a547c0705d3fec080ba921b700
tucq <492676375@qq.com>
2021-11-09 10:25:28 +0800

    test new branch

--- jeecg-boot-module-system/src/test/java/Test.java
+++ jeecg-boot-module-system/src/test/java/Test.java
@@ -4,11 +4,6 @@
 public class Test {
     public static void main(String[] args) {
 
-        long milliseconds = 951866 / 1000;
-        System.out.println(milliseconds);
-        System.out.println(milliseconds / (60 * 60));
-        System.out.println(milliseconds / (60));
-        System.out.println(milliseconds / (60));
-
+        System.out.println("test new branch");
     }
 }
